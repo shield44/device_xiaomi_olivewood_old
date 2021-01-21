f638b03dde4cc8206fb9aff8a82b39586532d33a
GeoPD <geoemmanuelpd2001@gmail.com>
2021-01-21 10:19:01 +0530

    olivewood: seccomp_policy: Remove mediacodec.policy
    -Added in vendor tree
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    Signed-off-by: shield44 <motoecondorxt1022@gmail.com>

--- device.mk
+++ device.mk
@@ -370,7 +370,6 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp_policy/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
 
 # Sensors
