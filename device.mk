dbfb0b1255409ee32ea5986ed4ae536a00598e27
Zinadin Zidan <tuhinsultana075@gmail.com>
2021-02-26 09:40:00 +0200

    olivewood: build more hidl packages
    Signed-off-by: ZunayedDihan <zunayeddihan@gmail.com>
    Signed-off-by: shield44 <motoecondorxt1022@gmail.com>

--- device.mk
+++ device.mk
@@ -182,7 +182,10 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system \
+    android.hidl.base@1.0_vendor
 
 # IMS
 PRODUCT_PACKAGES += \
