8beeca5ee89a743405c113523d7630f6ae5b2a7b
SebaUbuntu <barezzisebastiano@gmail.com>
2021-03-31 18:42:18 +0000

    olivewood: build atrace from source
    Signed-off-by: Alexander <yaalex@yaalex.tk>

--- device.mk
+++ device.mk
@@ -66,6 +66,10 @@ PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
