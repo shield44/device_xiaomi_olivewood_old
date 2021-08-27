f0db8e6e35ef63f1b4d186de1223f0d1aba2a47b
Northn <erolerol3760@gmail.com>
2021-08-27 11:33:57 +0000

    olivewood: Switch to oss camera hal

--- device.mk
+++ device.mk
@@ -88,6 +88,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    camera.msm8937 \
     libgui_vendor \
     libmm-qcamera \
     libdng_sdk.vendor \
