dd539e6698f0a9c6e8a98a76f24a4473bed15e85
Northn <erolerol3760@gmail.com>
2021-08-27 11:33:57 +0000

    olivewood: Switch to oss camera hal
    Signed-off-by: shield44 <motoecondorxt1022@gmail.com>

--- device.mk
+++ device.mk
@@ -113,11 +113,16 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     android.hardware.camera.device@3.6 \
     android.hardware.camera.provider@2.6 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libstdc++.vendor \
+    camera.msm8937 \
+    libmm-qcamera \
+    libdng_sdk.vendor \
     libgui_vendor \
     Snap
 
