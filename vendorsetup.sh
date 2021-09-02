2909294fdf22548ed49bea6cf54985cad4fedd84
shield44 <motoecondorxt1022@gmail.com>
2021-09-02 07:29:06 +0000

    olivewood: Use R Display and media hals
    Signed-off-by: shield44 <motoecondorxt1022@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,7 @@
-# Use PROD Camera Pixel Format
-cd hardware/qcom-caf/msm8996/display
-git fetch https://github.com/PixelExperience/hardware_qcom-caf_msm8996_display
-git cherry-pick 20a6dc8b24853cffda07e3b454503740bac79a4d
-cd ..
-# Use LineageOS audio hals
-rm -rf audio
+# Use R hals
+cd hardware/qcom-caf/msm8996/
+rm -rf media display media audio
 git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8996 audio
+git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_media media
+git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_display display
 cd ../../../
