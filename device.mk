a10b3c67d541ba94781469bcea0da7a4b987b2f6
pjgowtham <pjgowtham@users.noreply.github.com>
2021-09-10 13:27:58 +0530

    olivewood: build libgui_vendor from source
    Signed-off-by: shield44 <motoecondorxt1022@gmail.com>

--- device.mk
+++ device.mk
@@ -117,6 +117,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.6 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor \
+    libgui_vendor \
     Snap
 
 # Component overrides
