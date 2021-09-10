3b8c740df4ffff69e4cac77aa4ce33119ae0aef3
TheMalachite <TheMalachite@users.noreply.github.com>
2021-09-10 13:20:21 +0530

    olivewood: build camera stack from source
    * vendor/lib/libmmcamera_pppbase_module.so >> vendor/lib/libmmcamera_ppbase_module.so
    Signed-off-by: shield44 <motoecondorxt1022@gmail.com>

--- device.mk
+++ device.mk
@@ -111,6 +111,13 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.6 \
+    android.hardware.camera.provider@2.6 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
