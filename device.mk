62f2cae3c3ab19d0952bdcab6358689d738a7303
shield44 <motoecondorxt1022@gmail.com>
2021-09-20 12:52:31 +0000

    olivewood: move target kernel version to device makefile and remove ant hal permission copying
    * Also switch to BOARD_SEPOLICY_DIRS
    Signed-off-by: shield44 <motoecondorxt1022@gmail.com>

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ TARGET_SCREEN_WIDTH := 720
 # Board
 TARGET_BOARD_PLATFORM := msm8937
 
+# Kernel
+TARGET_KERNEL_VERSION := 4.9
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
@@ -251,7 +254,6 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
